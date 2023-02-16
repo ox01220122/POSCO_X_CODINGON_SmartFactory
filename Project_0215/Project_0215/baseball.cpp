c7ec1b382c8ad93f6ef8a4c34c116953a5aa4afc
ox01220122 <ox0122@gmail.com>
2023-02-17 01:29:28 +0900

    study

---
+++ Project_0215/Project_0215/baseball.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+int main() {
+	srand(time(NULL));
+
+	int com_num = 0, input_num = 0;
+	int com_arr[3];
+	
+	for(){
+		for(int c : com_arr)
+		com_num = (rand() % 3) + 1;
+		if (c == com_num)
+		{
+
+		}
+		
+	}
+
+
+
+
+	return 0;
+}
